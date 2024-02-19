7c92e0e12dfb0098c97336b5bd0e146f1ecff81d
AmirN347 <amirn2203@gmail.com>
2024-02-19 12:03:16 -0500

    Cleaned up code. Added errors messages to fail cases

--- matrixReducer.cpp
+++ matrixReducer.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 void printMatrix(int rows, int cols, vector<vector<double> > matrix);
@@ -15,7 +14,7 @@ bool checkRREF(vector<vector<double> > matrix);
 
 int main()
 {
-    // intilizing matrix
+    // initializing matrix
     int rows, cols;
     cout << "How many rows does the matrix have" << endl;
     cin >> rows;
@@ -116,66 +115,58 @@ bool checkRREF(vector<vector<double> > matrix)
     // track leading 1's column of previous iteration of row
     int prevLeading1Pos = -1;
     vector<double> currRow;
-    for (int i = 0; i < matrix.size(); i++)
-    {
-        
+    for (int i = 0; i < matrix.size(); i++) {
+
         currRow = matrix.at(i);
         // find leading 1 of that row
-        for (int j = 0; j < currRow.size(); j++)
-        {
-            if (currRow.at(j) == 1)
-            {
+        for (int j = 0; j < currRow.size(); j++) {
+            if (currRow.at(j) == 1) {
                 leading1Pos = j;
-                if(leading1Pos < prevLeading1Pos){
+                if (leading1Pos < prevLeading1Pos) {
+                    cout << "Found leading 1 before previous leading 1" << endl;
                     return false;
                 }
                 break;
-            }
-            else if (currRow.at(j) != 0)
-            {
+            } else if (currRow.at(j) != 0) {
                 return false;
             }
         }
 
 
 
-        // every number after 1 has to be 0's
-        if (leading1Pos != prevLeading1Pos && leading1Pos != -1)
-        {
+        //the position of leading 1 can't be the same position as the previous leading 1 and
+        // can't be negative(1 not found)
+        if (leading1Pos != prevLeading1Pos && leading1Pos != -1) {
             // checks if the column above is zero
-            
-            for (int z = i + 1; z < matrix.size(); z++)
-            {
-                if (matrix.at(z).at(leading1Pos) != 0)
-                {
-                    cout << "The leading 1's position is " << leading1Pos + 1 << endl;
+            for (int z = i + 1; z < matrix.size(); z++) {
+                if (matrix.at(z).at(leading1Pos) != 0) {
+                    cout << "The values above the leading 1 at position: " << leading1Pos << " are not zero" << endl;
                     return false;
                 }
             }
 
             //checks if column above is zero
 
-            for(int j = i-1; j>=0;j--){
-               
-                if(matrix.at(j).at(leading1Pos) != 0 ){
-                    
+            for (int j = i - 1; j >= 0; j--) {
+
+                if (matrix.at(j).at(leading1Pos) != 0) {
+                    cout << "The values below the leading 1 at position: " << leading1Pos << " are not zero" << endl;
                     return false;
                 }
             }
         }
-        else
-        {
-            for (int z = 0; z < currRow.size(); z++)
-            {
-                if (currRow.at(z) != 0)
-                {
+            //if no leading 1 is found make sure row is all zero's
+        else {
+            for (int z = 0; z < currRow.size(); z++) {
+                if (currRow.at(z) != 0) {
+                    cout << "The entire row is not zero's" << endl;
                     return false;
                 }
             }
         }
-       
+        //setting previous leading 1 pos as the current one
         prevLeading1Pos = leading1Pos;
-        
+
     }
     return true;
 }
